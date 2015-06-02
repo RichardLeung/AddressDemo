9b9f31010f6a0958aa5eb8ea4bcc1d31dd17243b
Richard Leung <lyskr@126.com>
2015-06-03 00:47:55 +0800

    Initial Commit

---
+++ AddressDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AddressDemo
+//
+//  Created by 梁原 on 15/6/3.
+//  Copyright (c) 2015年 RL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AddressDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  AddressDemo
+//
+//  Created by 梁原 on 15/6/3.
+//  Copyright (c) 2015年 RL. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
